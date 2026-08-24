87c42632fed09232781e04a52e0f0b039035faca
Aenae <n.lenouveau@gmail.com>
2026-08-24 22:34:06 +0200

    test pour cookie

--- back/src/server.ts
+++ back/src/server.ts
@@ -42,6 +42,7 @@ app.use(cors({
   credentials: true,
 }));
 
+console.log("NODE_ENV =", process.env.NODE_ENV);
 app.use(session({
   secret: process.env.SESSION_SECRET || "dev-secret",
   resave: false,
