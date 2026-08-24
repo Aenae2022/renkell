a0669f54f7e75a8e26ee845592729ed631922ec6
Aenae <n.lenouveau@gmail.com>
2026-08-24 22:08:03 +0200

    Fix production session cookie

--- back/src/server.ts
+++ back/src/server.ts
@@ -50,7 +50,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 1000 * 60 * 30, // 30 min
   }
 }));
