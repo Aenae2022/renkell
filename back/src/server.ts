45fb915dda98adbb3261e8dff4375256ced335eb
Aenae <n.lenouveau@gmail.com>
2026-08-24 21:54:31 +0200

    reparation server.ts

--- back/src/server.ts
+++ back/src/server.ts
@@ -24,7 +24,8 @@ const app = express();
 // app.use(cors({
 //   origin: "http://localhost:5173", // <-- ton frontend
 //   credentials: true               // <-- autorise les cookies
-// }));app.use(express.json());
+// }));
+app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:4173",
